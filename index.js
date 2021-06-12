b9865a34a30687ccc4f6e5ea686f2919bc6d54ec
Matt Nolan <menolan22@gmail.com>
2021-06-12 11:08:37 -0400

    second commit, stories completed up to modify the range. W.I.P.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//import the readline library for cleaner output in the terminal
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -6,16 +7,82 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
-
+//random number generator
+function randNum(min, max) {
+  let range = max - min + 1;
+  return Math.floor(Math.random() * range) + min
+}
+let min = 1
+let max = 100
 start();
-
-// here we goooo
-
-
+// have the computer greet the user and ask if they would like to play
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
   // Now try and complete the program.
-  process.exit();
+
+  // the game!
+
+  let number = randNum(min, max)
+  let answer = await ask(`Is your number ${number}? `)
+  let highLow;
+  // binary search algo to find the number in as few guesses as possible (thanks youTube!)
+  const binary = (val, arr) => {
+    let lower = min
+    let upper = max;
+
+    while (lower <= upper) {
+      const middle = lower + Math.floor((max - min) / 2);
+
+      if (val === arr[middle]) {
+        return middle;
+      }
+
+      if (val < arr[middle]) {
+        upper = middle - 1;
+      } else {
+        lower = middle + 1
+      }
+    }
+  }
+    
+
+  while (answer === "n") {
+    console.log("asking higher or lower")
+    highLow = await ask(`Is your number higher(h) or lower(l) than ${number}? `)
+
+
+    console.log("asking higher or lower")
+
+    if (highLow === "h") {
+      min = number + 1
+      number = randNum(min, max)
+
+      console.log("making new number and asking again")
+      console.log(answer)
+
+      answer = await ask(`Is your number ${number}? `)
+
+      console.log(answer)
+
+
+
+    } else if (highLow === "l") {
+      max = number - 1
+      number = randNum(min, max)
+
+
+      answer = await ask(`Is your number ${number}? `)
+
+
+    }
+
+  }
+  if (answer === "y") {
+
+    console.log("I WIN!!!")
+    process.exit()
+  }
 }
+start()                        
